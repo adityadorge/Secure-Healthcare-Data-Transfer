c4a9c096a05bae312d1d6da0e7f3f00181ce840e
Pranav Dalvi <70306291+PranavDalvi@users.noreply.github.com>
2024-05-02 23:53:45 +0530

    Add TOTP (#22)
    * implementing all auth authentication system
    
    * new auth system implementation
    
    * update css and now admins will redirected to admin dashboard
    
    * code cleanup and fix auth notifications
    
    * add 2fa but google sign-in is bugging
    
    * add totp custom pages

--- SHDTsys/user_auth/admin.py
+++ SHDTsys/user_auth/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import User
-# , UserDetail
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
@@ -30,5 +29,10 @@ class UserAdmin(BaseUserAdmin):
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
 
+
+# Ensure users go through the allauth workflow when logging into admin.
+admin.site.login = staff_member_required(admin.site.login, login_url='/accounts/login')
+# Run the standard admin set-up.
+admin.autodiscover()
+
 admin.site.register(User, UserAdmin)
-# admin.site.register(UserDetail)
--- SHDTsys/user_auth/urls.py
+++ SHDTsys/user_auth/urls.py
@@ -4,8 +4,4 @@ from . import views
 # app_name = "user_auth"
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
-    # path('accounts/register/', views.register_user, name='register'),
-    # path('profile_settings/', views.user_detail, name='user_detail')
 ]
