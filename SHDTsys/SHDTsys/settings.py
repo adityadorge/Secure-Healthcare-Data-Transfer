a1351eb6f9cededd42742ba818f3cdeb49210968
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-05-04 19:30:30 +0530

    fixed google Oauth

--- SHDTsys/SHDTsys/settings.py
+++ SHDTsys/SHDTsys/settings.py
@@ -196,5 +196,4 @@ EMAIL_USE_TLS = True
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-ACCOUNT_ADAPTER = "allauth_2fa.adapter.OTPAdapter"
-# ACCOUNT_ADAPTER = "user_auth.custom_adapter.CustomAccountAdapter"
+ACCOUNT_ADAPTER = "user_auth.custom_adapter.CustomAccountAdapter"
