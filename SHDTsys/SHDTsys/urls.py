94e8732687e3877cda50772093ed82310f925849
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-05-07 12:28:55 +0530

    testing other mfa

--- SHDTsys/SHDTsys/urls.py
+++ SHDTsys/SHDTsys/urls.py
@@ -6,6 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
-    path('accounts/two-factor/', include('allauth_2fa.urls')),
     path('', include('user_auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
