8ca12a7f2bb00200531b1a02410878cc22891841
Aditya Dorge <72189514+adityadorge@users.noreply.github.com>
2024-05-10 14:43:58 +0530

    Delete SHDT-env directory

--- SHDT-env/lib/python3.10/site-packages/allauth/account/internal/flows/reauthentication.py
+++
@@ -1,5 +0,0 @@
-from allauth.account.authentication import record_authentication
-
-
-def reauthenticate_by_password(request):
-    record_authentication(request, method="password", reauthenticated=True)
